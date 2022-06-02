944afc0047849472f9f9a52a24c6117c5bb51dc8
stewart <16131485+StewartYe@users.noreply.github.com>
2022-06-02 18:58:40 +0800

    update transaction version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 117,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 	state_version: 1,
 };
 
