8abcbc76c1e628bb740bd090131ebd2ec456e68e
Kachinsky <22611640+kachinskyinsc2@users.noreply.github.com>
2023-03-31 10:04:57 +0800

    add put-into-vault transaction (#67)
    Co-authored-by: Cyberaurora <22611640+cyberaurora@users.noreply.github.com>

--- pallets/foundation/src/tests.rs
+++ pallets/foundation/src/tests.rs
@@ -2,7 +2,7 @@ use crate::mock::*;
 use crate::FoundationData;
 use crate::Pallet;
 use frame_support::dispatch::Weight;
-use frame_support::traits::OnInitialize;
+use frame_support::traits::{OnFinalize, OnInitialize};
 use frame_system::AccountInfo;
 use sp_keyring::AccountKeyring;
 
@@ -12,7 +12,6 @@ type Foundation = Pallet<Test>;
 fn test_foundation() {
     new_test_ext().execute_with(|| {
         let alice: AccountId = AccountKeyring::Alice.into();
-        frame_system::Pallet::<Test>::set_block_number(30);
         let alice_balance: AccountInfo<u64, pallet_balances::AccountData<u128>> =
             frame_system::Pallet::<Test>::account(&alice);
         assert_eq!(alice_balance.data.reserved, 2000000000000000000000);
@@ -31,11 +30,11 @@ fn test_foundation() {
             }
         );
 
-        let weight = Foundation::on_initialize(5);
+        let weight = run_to_block(5);
         assert!(weight.eq(&Weight::from_ref_time(0u64)));
-        let weight = Foundation::on_initialize(15);
+        let weight = run_to_block(15);
         assert!(weight.eq(&Weight::from_ref_time(0u64)));
-        Foundation::on_initialize(20);
+        run_to_block(20);
         let alice_foundation = Foundation::foundation(&alice);
         assert_eq!(
             alice_foundation.unwrap(),
@@ -43,8 +42,8 @@ fn test_foundation() {
                 delay_durations: 2,
                 interval_durations: 1,
                 times: 5,
-                amount: 300000000000000000000,
-                first_amount: 500000000000000000000
+                amount: 300_000000000_000000000,
+                first_amount: 500_000000000_000000000
             }
         );
         let alice_balance: AccountInfo<u64, pallet_balances::AccountData<u128>> =
@@ -52,10 +51,33 @@ fn test_foundation() {
         assert_eq!(alice_balance.data.reserved, 1500000000000000000000);
         assert_eq!(alice_balance.data.free, 500000000000000000000);
 
-        Foundation::on_initialize(30);
-        Foundation::on_initialize(40);
-        Foundation::on_initialize(50);
-        Foundation::on_initialize(60);
+        let bob: AccountId = AccountKeyring::Bob.into();
+        let ferdie: AccountId = AccountKeyring::Ferdie.into();
+        assert!(Foundation::put_into_vault(
+            RuntimeOrigin::signed(ferdie.clone()),
+            bob.clone(),
+            FoundationData {
+                delay_durations: 3,
+                interval_durations: 1,
+                times: 5,
+                amount: 83333_333333333_333333333,
+                first_amount: 83333_333333333_333333333,
+            },
+        )
+        .is_ok());
+        assert_eq!(0, Balances::free_balance(&bob));
+        assert_eq!(499999_999999999_999999998, Balances::reserved_balance(&bob));
+        run_to_block(21);
+        assert_eq!(0, Balances::free_balance(&bob));
+        assert_eq!(499999_999999999_999999998, Balances::reserved_balance(&bob));
+        run_to_block(30);
+        assert_eq!(83333_333333333_333333333, Balances::free_balance(&bob));
+        assert_eq!(416666_666666666_666666665, Balances::reserved_balance(&bob));
+        run_to_block(40);
+        assert_eq!(166666_666666666_666666666, Balances::free_balance(&bob));
+        assert_eq!(333333_333333333_333333332, Balances::reserved_balance(&bob));
+        run_to_block(50);
+        run_to_block(60);
         let alice_foundation_data = Foundation::foundation(&alice);
         assert!(alice_foundation_data.is_some());
         let alice_foundation = Foundation::foundation(&alice);
@@ -70,7 +92,7 @@ fn test_foundation() {
             }
         );
 
-        Foundation::on_initialize(70);
+        run_to_block(70);
         let alice_foundation_data = Foundation::foundation(&alice);
         assert!(alice_foundation_data.is_none());
         let alice_balance: AccountInfo<u64, pallet_balances::AccountData<u128>> =
@@ -78,8 +100,6 @@ fn test_foundation() {
         assert_eq!(alice_balance.data.free, 2000000000000000000000);
         assert_eq!(alice_balance.data.reserved, 0);
 
-        let weight = Foundation::on_initialize(101);
-        assert!(weight.eq(&Weight::from_ref_time(0u64)));
         let alice_foundation_data = Foundation::foundation(&alice);
         assert!(alice_foundation_data.is_none());
         let alice_balance: AccountInfo<u64, pallet_balances::AccountData<u128>> =
@@ -87,4 +107,19 @@ fn test_foundation() {
         assert_eq!(alice_balance.data.free, 2000000000000000000000);
         assert_eq!(alice_balance.data.reserved, 0);
     });
+}
+
+fn run_to_block(n: u32) -> Weight {
+    while System::block_number() < n {
+        if System::block_number() > 1 {
+            System::on_finalize(System::block_number());
+        }
+        System::set_block_number(System::block_number() + 1);
+        System::on_initialize(System::block_number());
+        Foundation::on_initialize(System::block_number());
+    }
+    System::on_finalize(System::block_number());
+    System::set_block_number(System::block_number() + 1);
+    System::on_initialize(System::block_number());
+    Foundation::on_initialize(System::block_number())
 }
